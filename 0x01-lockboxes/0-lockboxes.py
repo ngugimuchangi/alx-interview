af1a9bb55a20f3d7d6145adb1ae9729c4d4f7e8b
ngugimuchangi <tech.curious14@gmail.com>
2023-03-11 11:51:33 +0300

    locked boxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lockboxes 🔒
+"""
+
+
+def canUnlockAll(boxes):
+    """ Lockboxes function
+        Args:
+            - boxes: list of boxes either empty
+                     or with one or more keys.
+        Return:
+            - True or False depending on whether
+              all the boxes can be unlocked based
+              keys found and where found
+    """
+    if not boxes:
+        return False
+    if len(boxes) == 1:
+        return True
+    unlock_status = {x: True if not x else False for x in range(len(boxes))}
+    for box in boxes:
+        viable_keys = list(filter(lambda x: all([x > 0, x < len(boxes),
+                                                 x != boxes.index(box)]), box))
+        unlock_status.update({x: True for x in viable_keys})
+    return all(unlock_status.values())
--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - Coding interview questions
 
 ## Projects
-- #### [0x00. Pascal's Triangle](0x00-pascal_triangle) 
+- #### [0x00. Pascal's Triangle](0x00-pascal_triangle)
+- #### [0x01. Lockboxes](0x01-lockboxes)
 - #### [0x06. Star Wars API](0x06-starwars_api)
