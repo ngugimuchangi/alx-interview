9229389191a5cd183efcd0cd581e5b801ec307c9
ngugimuchangi <tech.curious14@gmail.com>
2023-03-17 01:50:29 +0300

    requiremnt fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ 2D Matrix rotation challenge
 """
+from typing import List
 
 
-def rotate_2d_matrix(matrix: list[list]):
+def rotate_2d_matrix(matrix: List[List]) -> None:
     """ Rotates 2D matrix 90 decrease clockwise
         in place
         Args:
@@ -12,7 +13,7 @@ def rotate_2d_matrix(matrix: list[list]):
     inward_cycles = int(len(matrix) // 2)
     last_idx = len(matrix) - 1
     for i in range(inward_cycles):
-        for j in range(i, len(matrix) - 1 - i):
+        for j in range(i, last_idx - i):
             matrix[i][j], matrix[j][last_idx - i] = \
                 matrix[j][last_idx - i], matrix[i][j]
             matrix[i][j], matrix[last_idx - j][i] = \
