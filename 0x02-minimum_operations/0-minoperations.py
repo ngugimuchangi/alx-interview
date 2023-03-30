e6d25e3e860e1da04cc83a3558067893f6214a70
ngugimuchangi <tech.curious14@gmail.com>
2023-03-30 22:40:42 +0300

    README.md and code init file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Minimum operations questions
+"""
+
+
+def minOperations(n: int) -> int:
+    """ Finds the minimum operations needed
+        to result in exactly nH characters in
+        a file
+    """
+    pass
