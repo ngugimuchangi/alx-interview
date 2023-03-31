4925fd0d9e361ee2f9cee87a2e5f8ce55285a066
ngugimuchangi <tech.curious14@gmail.com>
2023-03-31 12:57:02 +0300

    minor code refactoring

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,25 +8,23 @@ def minOperations(n: int) -> int:
         to result in exactly nH characters in
         a file
     """
-    if n <= 0:
-        return 0
     available_characters = 1
     pending_characters = n - 1
     copied_characters = 0
     ops = 0
 
-    while (pending_characters):
+    while (pending_characters > 0):
         if copied_characters and pending_characters % available_characters:
             pending_characters -= copied_characters
             available_characters += copied_characters
             ops += 1
             continue
-        if available_characters <= pending_characters:
+        if available_characters < pending_characters:
             copied_characters = available_characters
         else:
             copied_characters = pending_characters
-        pending_characters -= copied_characters
         available_characters += copied_characters
+        pending_characters -= copied_characters
         ops = ops + 2
 
     if available_characters == n:
