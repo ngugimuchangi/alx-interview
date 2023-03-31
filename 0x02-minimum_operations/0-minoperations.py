e884909307d86f3df1abfb8dcb812af06fab79ca
ngugimuchangi <tech.curious14@gmail.com>
2023-03-31 12:07:28 +0300

    minimum ops task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,4 +8,27 @@ def minOperations(n: int) -> int:
         to result in exactly nH characters in
         a file
     """
-    pass
+    if not n or n == 1:
+        return 0
+    available_characters = 1
+    pending_characters = n - 1
+    copied_characters = 0
+    ops = 0
+
+    while (pending_characters):
+        if copied_characters and pending_characters % available_characters:
+            pending_characters -= copied_characters
+            available_characters += copied_characters
+            ops += 1
+            continue
+        if available_characters <= pending_characters:
+            copied_characters = available_characters
+        else:
+            copied_characters = pending_characters
+        pending_characters -= copied_characters
+        available_characters += copied_characters
+        ops = ops + 2
+
+    if available_characters == n:
+        return ops
+    return 0
