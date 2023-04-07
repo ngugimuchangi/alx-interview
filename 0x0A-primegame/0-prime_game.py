ffe0dcfdfda63290d33d74df6dad3cbbcc364362
ngugimuchangi <tech.curious14@gmail.com>
2023-04-07 05:54:46 +0300

    prototype and README

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Prime Game Problem
+    Topic: Eratosthenes algorithm
+"""
+from typing import List
+
+
+def isWinner(x: int, nums: List[int]) -> str:
+    """
+    Determines the winner in the prime game using
+    Eratosthenes prime sieving algorithm
+    """
+    return "Ben"
