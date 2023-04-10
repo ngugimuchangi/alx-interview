b1d5d823df0e68af537170e8d171c57b860ae773
ngugimuchangi <tech.curious14@gmail.com>
2023-04-10 14:51:37 +0300

    fix log format check

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,7 +20,7 @@ def validate_format(log):
     """
     Validates log format
     """
-    return False if len(log.split()) < 8 else True
+    return False if len(log.split()) < 7 else True
 
 
 def print_log(file_size, status_codes) -> None:
